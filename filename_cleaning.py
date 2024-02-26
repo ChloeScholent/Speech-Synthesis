465db076564eb05097d8b28a0650f507f3aae1fc
ChloeScholent <159830563+ChloeScholent@users.noreply.github.com>
2024-02-26 12:42:28 +0000

    Update filename_cleaning.py

--- filename_cleaning.py
+++ filename_cleaning.py
@@ -1,15 +1,12 @@
-#Cleaning the recording data by suppressing number of takes.
+#Cleaning the recording data by suppressing the number of takes.
 
 import os
 
-#Open folder
 folder = r'C:\Users\Chloé\Desktop\chloe arctica\\'
 
-#Determine the string to be renamed
 for file_name in os.listdir(folder):
     source = folder + file_name
     destination = file_name.replace("_1", "") 
-
     os.rename(source, destination)
  
 
